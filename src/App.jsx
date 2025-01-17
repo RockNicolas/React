17f67417b6518b4738c60caefc204d3031ddeb52
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-01-17 16:12:35 -0300

    Criando o componente Filter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import Todo from './components/Todo'; 
 import TodoForm from './components/TodoForm'; 
 import Search from './components/Search';
+import Filter from './components/filter';
 import './App.css';
+
 
 function App() {
   const [todos, setTodos] = useState([
@@ -27,6 +29,9 @@ function App() {
   ]);
 
   const [ search ,setSearch ] = useState("");
+
+  const [filter ,setFilter ] = useState("All");
+  const[sort, setSort] = useState("Asc");
 
   const addTodo = (text, category,) => {
     const newTodos = [
@@ -60,10 +65,26 @@ function App() {
     <div className="app">
       <h1>Lista de tarefas</h1>
       <Search search={search} setSearch={setSearch}/>
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
       <div className="todo-list">
-        {todos.filter((todo) => 
-        todo.text.toLowerCase().includes(search.toLowerCase())
+        {todos
+        .filter((todo) =>
+          filter === "All"
+          ? true
+          : filter === "Completed"
+          ? todo.isCompleted 
+          : !todo.isCompleted
+        )   
+        .filter((todo) => 
+          todo.text.toLowerCase().includes(search.toLowerCase())
         )
+
+        .sort((a, b) => 
+         sort === "Asc" 
+          ? a.text.localeCompare(b.text) 
+          : b.text.localeCompare(a.text)
+        ) 
+        
         .map((todo) => (
           <Todo 
             key={todo.id}
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const filter = () => {
+const filter = ({ filter, setFilter, setSort }) => {
   return (
-    <div>filter</div>
+    <div className='filter'>
+        <h2>Filtrar:</h2>
+        <div className="filter-options">
+            <div>
+                <p>Status:</p>
+                <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                    <option value="All">Todas</option>
+                    <option value="Completed">Completas</option>  
+                    <option value="Incompleted">Incompletas</option>
+                </select>
+            </div>
+            <div>
+                <p>Ordem alfabetica:</p>
+                <button onClick={() => setSort("Asc")}>Asc</button>
+                <button onClick={() => setSort("Desc")}>Desc</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
